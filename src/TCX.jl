e18f9e189d37ba529d2f9383f095eb357f9e0213
brotherjack <brotherjack1877@gmail.com>
2020-06-18 23:00:24 -0400

    added node names to make paths easier to read and a parseNode function to make code more DRY

--- src/TCX.jl
+++ src/TCX.jl
@@ -47,7 +47,7 @@ function parse_tcx_file(file::String)
     end
 
     # Type - "/*/*[1]/*[1]/@Sport"
-    aType = nodecontent(findfirst("/*/*[1]/*[1]/@Sport", xmldoc))  
+    aType = nodecontent(findfirst("/*/*[1]/*[1]/@Sport", xmldoc))
     # Id - "/*/*[1]/*/*[1]"
     xid = nodecontent(findfirst("/*/*[1]/*/*[1]", xmldoc))
 
@@ -60,38 +60,43 @@ function parse_tcx_file(file::String)
     aDistance = parse(Float64, nodecontent(findfirst("/*/*[1]/*/*[2]/*[2]", xmldoc)))
     # DistanceMeters - "/*/*[1]/*/*[2]/*[2]"
     # AverageHeartRateBpm - "/*/*[1]/*/*[2]/*[5]/*[1]"
-    xbpm = findfirst("/*/*[1]/*/*[2]/*[5]/*[1]", xmldoc) 
+    xbpm = findfirst("/*/*[1]/*/*[2]/*[5]/*[1]", xmldoc)
     if xbpm === nothing
         aHeartRateBpm = 0
     else
         aHeartRateBpm = parse(Int32, nodecontent(xbpm))
     end
     # TrackPoints - "/*/*[1]/*/*[2]/*[9]/*"
-    tp_Points = findall("/*/*[1]/*/*[2]/*[9]/*", xmldoc)   
+    tp_Points = findall("/*/*[1]/*/*[2]/*[9]/*", xmldoc)
     aTrackPoints = Array{TrackPoint, size(tp_Points, 1)}[]
     for tp in tp_Points
-        xtime = nodecontent(findfirst("./*[1]", tp))
+        xtime = nodecontent(findfirst("./*[local-name()='Time']", tp))
         tp_time = convertToDateTime(xtime)
-        xlat = findfirst("./*[2]/*[1]", tp)
-        if xlat !== nothing
-            tp_lat = parse(Float64, nodecontent(xlat))
-        else
-            continue
-        end
-        tp_lont = parse(Float64, nodecontent(findfirst("./*[2]/*[2]", tp)))
-        xbpm = findfirst("./*[5]/*[1]", tp)
-        if xbpm !== nothing
-            tp_bpm = parse(Int32, nodecontent(findfirst("./*[5]/*[1]", tp)))
-        else
-            tp_bm = 0
-        end
-        tp_dist = parse(Float64, nodecontent(findfirst("./*[3]", tp)))
-        tp_alt = parse(Float64, nodecontent(findfirst("./*[4]", tp)))
+        tp_lat = parseNode(Float64, "./*[local-name()='Position']/*[local-name()='LatitudeDegrees']", tp)
+        tp_lont = parseNode(Float64, "./*[local-name()='Position']/*[local-name()='LatitudeDegrees']", tp)
+        tp_bpm = parseNode(Int32, "./*[local-name()='HeartRateBpm']/*[1]", tp)
+        tp_dist = parseNode(Float64, "./*[local-name()='TPX']", tp)
+        tp_alt = parseNode(Float64, "./*[local-name()='AltitudeMeters']", tp)
 
         aTrackPoints = vcat(aTrackPoints, TrackPoint(tp_time, tp_lat, tp_lont, tp_bpm, tp_dist, tp_alt))
     end
 
     return 200, TCXRecord(aId, aName, aType, aDistance, aTime, aHeartRateBpm, aTrackPoints)
+end
+
+#=
+= Parses an XML node based on an XPATH and data type.
+=
+= If the node is a `nothing` value, the function returns the data
+= type's version of 0
+=#
+function parseNode(dType, path, node)
+    node_check = findfirst(path, node)
+    if node_check !== nothing
+        return parse(dType, nodecontent(node_check))
+    else
+        return dType(0)
+    end
 end
 
 function parse_tcx_dir(path::String)

045c8e0d2c92744272107ca5b27c7c0da2390c1e
brotherjack <brotherjack1877@gmail.com>
2020-09-05 17:59:46 -0400

    added a test for when parse_tcx_dir is passed a string that is not a path

--- test/test_tcx_read_dir.jl
+++ test/test_tcx_read_dir.jl
@@ -1,6 +1,11 @@
 using Test
 
 @testset "TESTSETS: Test read TCX dir" begin
+    @testset "CASE: Test a string that is NOT a path" begin
+        err, _ = TCX.parse_tcx_dir("I am not a path")
+        @test err == TCX.SERVER_ERROR
+    end
+
     @testset "CASE: Test dir with NO TCX file" begin
 	      err, _ =  TCX.parse_tcx_dir(tempdir())
         @test err == TCX.NOT_FOUND
@@ -15,6 +20,7 @@ using Test
         err, _ =  TCX.parse_tcx_dir(".")
         @test err == TCX.OK
     end
+
     @testset "CASE: Test DataFrame after process dir" begin
         err, ta =  TCX.parse_tcx_dir(".")
         @test (err == TCX.OK) & (size(getDataFrame(ta), 1) == 10087)
